a742d62c4509adcaab672ab67b8fda8435786a2a
PaulJKathmann <paul.kathmann.98@gmail.com>
2026-02-14 17:16:10 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,13 @@
 import OpenAI from "openai";
+import fs from "fs";
+import type { ChatCompletionMessage } from "openai/resources/chat/completions/completions.js";
+
 
 const tools = [
       {
         type: "function",
         function: {
-          name: "ReadFile",
+          name: "read_file",
           description: "Read and return the contents of a file",
           parameters: {
             type: "object",
@@ -19,6 +22,15 @@ const tools = [
         },
       }
   ];
+
+function readFile({ file_path }: { file_path: string }): string {
+  try {
+    const data = fs.readFileSync(file_path, "utf8");
+    return data;
+  } catch (err) {
+    return `Error reading file: ${err}`;
+  }
+}
 
 async function main() {
   const [, , flag, prompt] = process.argv;
@@ -41,8 +53,9 @@ async function main() {
 
   const response = await client.chat.completions.create({
     model: "anthropic/claude-haiku-4.5",
+    tools,
     messages: [{ role: "user", content: prompt }],
-    tools: tools
+   
   });
 
   if (!response.choices || response.choices.length === 0) {
@@ -53,6 +66,20 @@ async function main() {
   console.error("Logs from your program will appear here!");
 
   console.log(response.choices[0].message.content);
+
+  const message: ChatCompletionMessage = response.choices[0].message
+  const toolCalls: any[] = message.tool_calls ?? []
+  for (const toolCall of toolCalls) {
+    const tool_id = toolCall.id
+    const tool_name = toolCall.function.name
+    const tool_args = toolCall.function.arguments
+    console.log(`Tool called - \nID: ${tool_id} \nName: ${tool_name} \nArguments: ${JSON.stringify(tool_args)}`);
+    if (tool_name === "read_file") {
+      const result = readFile(tool_args);
+      console.log(`Tool result: ${result}`);
+    }
+  }
+
 }
 
 main();

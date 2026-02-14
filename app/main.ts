7d6a5c1712ffc3727be28715e6784f88e9c8a7a6
PaulJKathmann <paul.kathmann.98@gmail.com>
2026-02-14 17:26:32 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -77,7 +77,7 @@ async function main() {
     //console.log(`Tool called - \nID: ${tool_id} \nName: ${tool_name} \nArguments: ${JSON.stringify(tool_args)}`);
     if (tool_name === "read_file") {
       const result = readFile(file_path);
-      console.log(`${result}`);
+      console.log(result);
     }
   }
 
